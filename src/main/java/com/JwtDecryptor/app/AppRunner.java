fa0a60d47279d56b772f43715abc9c58b5861720
Rafekpro <codethegloobe@gmail.com>
2023-10-16 21:27:17 +0200

    not refactored yet

--- src/main/java/com/JwtDecryptor/app/AppRunner.java
+++ src/main/java/com/JwtDecryptor/app/AppRunner.java
@@ -1,78 +1,121 @@
 package com.JwtDecryptor.app;
 
 import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.JwtDecryptor.app.JWTdecoder.decodeJWT;
 import static com.JwtDecryptor.app.JWTgenerator.generateJWTToken;
 
-public class AppRunner
-{
+public class AppRunner {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         JFrame frame = new JFrame("JWT Token Decoder/Generator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(700, 400);
+        frame.setSize(600, 400);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
+        JPanel panel = new JPanel(new BorderLayout());
+
+        JPanel inputPanel = new JPanel(new GridLayout(3, 2, 10, 10));
+        inputPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         JLabel emailLabel = new JLabel("Email:");
         JTextField emailField = new JTextField(20);
 
-        JLabel usernameLabel = new JLabel("Username:");
-        JTextField usernameField = new JTextField(20);
+        JLabel roleLabel = new JLabel("Role:");
+        JTextField roleField = new JTextField(20);
 
+        inputPanel.add(emailLabel);
+        inputPanel.add(emailField);
+        inputPanel.add(roleLabel);
+        inputPanel.add(roleField);
 
         JTextArea inputTextArea = new JTextArea(5, 40);
-
-        JTextArea outputTextArea = new JTextArea(5, 40);
+        JTextPane outputTextArea = new JTextPane();
         outputTextArea.setEditable(false);
 
         JButton generateButton = new JButton("Generate JWT Token");
         JButton decodeButton = new JButton("Decode JWT");
-
+        JButton clearButton = new JButton("Clear Output");
 
         generateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String email = emailField.getText();
-                String username = usernameField.getText();
-                String jwtToken = generateJWTToken(email, username);
+                String role = roleField.getText();
+                String jwtToken = generateJWTToken(email, role);
                 inputTextArea.setText(jwtToken);
             }
         });
-
 
         decodeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String jwtToken = inputTextArea.getText().trim();
                 String decodedPayload = decodeJWT(jwtToken);
-                outputTextArea.setText(decodedPayload);
+                displayColorizedPayload(decodedPayload, outputTextArea);
             }
         });
 
-        JPanel buttonsPanel = new JPanel();
-        buttonsPanel.add(generateButton);
-        buttonsPanel.add(decodeButton);
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                outputTextArea.setText("");
+            }
+        });
 
-        JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new GridLayout(2, 2));
-        inputPanel.add(emailLabel);
-        inputPanel.add(emailField);
-        inputPanel.add(usernameLabel);
-        inputPanel.add(usernameField);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(generateButton);
+        buttonPanel.add(decodeButton);
+        buttonPanel.add(clearButton);
 
-        panel.add(inputPanel, BorderLayout.EAST);
-        panel.add(new JScrollPane(inputTextArea), BorderLayout.NORTH);
-       panel.add(buttonsPanel, BorderLayout.CENTER);
-       panel.add(new JScrollPane(outputTextArea), BorderLayout.SOUTH);
+        panel.add(inputPanel, BorderLayout.NORTH);
+        panel.add(new JScrollPane(inputTextArea), BorderLayout.CENTER);
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+        panel.add(new JScrollPane(outputTextArea), BorderLayout.EAST);
 
         frame.add(panel);
         frame.setVisible(true);
     }
 
+    private static void displayColorizedPayload(String payload, JTextPane textPane) {
+        StyledDocument doc = textPane.getStyledDocument();
+
+        SimpleAttributeSet redStyle = new SimpleAttributeSet();
+        StyleConstants.setForeground(redStyle, Color.RED);
+
+        SimpleAttributeSet blueStyle = new SimpleAttributeSet();
+        StyleConstants.setForeground(blueStyle, Color.BLUE);
+
+        Pattern algPattern = Pattern.compile("\"alg\":\\s*\"([^\"]+)\"");
+        Matcher matcher = algPattern.matcher(payload);
+
+        try {
+            int currentIndex = 0;
+
+            while (matcher.find()) {
+                int start = matcher.start() - 1;
+                int end = matcher.end() + 1;
+                doc.insertString(currentIndex, payload.substring(currentIndex, start), blueStyle);
+                doc.insertString(currentIndex + start, payload.substring(start, end), redStyle);
+                currentIndex = end;
+            }
+
+            doc.insertString(currentIndex, payload.substring(currentIndex), blueStyle);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/JwtDecryptor/app/JWTgenerator.java
+++ src/main/java/com/JwtDecryptor/app/JWTgenerator.java
@@ -5,13 +5,13 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 public class JWTgenerator {
-    public static String generateJWTToken(String email, String username) {
+    public static String generateJWTToken(String email,  String role) {
         // Generate a secret key for signing the JWT
         var secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
         return Jwts.builder()
                 .claim("email", email)
-                .claim("username", username)
+                .claim("role",role)
                 .signWith(secretKey)
                 .compact();
     }
